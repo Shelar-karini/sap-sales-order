55354467235f87c0a985a04c0c8f3bdd9977c0f8
Shelar-karini <aditya.shelar@karini.ai>
2026-03-05 13:37:01 +0530

    add root route

--- server.js
+++ server.js
@@ -1,21 +1,12 @@
 const cds = require('@sap/cds');
 
 cds.on('bootstrap', (app) => {
-  app.get('/', (req, res) => {
-    res.json({
-      status: '🟢 Running',
-      service: 'SAP Sales Order OData API',
-      version: '1.0.0',
-      endpoints: {
-        metadata          : '/odata/v4/$metadata',
-        VBAK              : '/odata/v4/VBAK',
-        VBAP              : '/odata/v4/VBAP',
-        VBKD              : '/odata/v4/VBKD',
-        VBPA              : '/odata/v4/VBPA',
-        VBUK              : '/odata/v4/VBUK'
-      }
-    });
+  app.use((req, res, next) => {
+    if (req.url && req.url.includes('+')) {
+      console.log('Original URL:', req.url);
+      req.url = req.url.replace(/\+/g, '%20');
+      console.log('Fixed URL:', req.url);
+    }
+    next();
   });
 });
-
-module.exports = cds.server;
